b6f4114a0e338f38e2e48cbcb697bac61592b2c8
airtin010 <ailtonpereira01010@gmail.com>
2026-02-28 16:50:58 -0300

    read.py update.

--- CRUDEMANAGER/read.py
+++ CRUDEMANAGER/read.py
@@ -3,13 +3,14 @@ from psycopg2 import Error
 
 
 class readuser:
-    def __init__(self, user_id):
+    def __init__(self, user_id, table):
         self.user_id = user_id
+        self.table = table
 
     def read(self, connection):
         try:
             cursor = connection.cursor()
-            select_query = "SELECT id, name, email FROM users WHERE id = %s"
+            select_query = f"SELECT id, name, email FROM {self.table} WHERE id = %s"
             cursor.execute(select_query, (self.user_id,))
             user = cursor.fetchone()
             if user:
